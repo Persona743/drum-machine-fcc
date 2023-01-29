d4b07e0c44ec2ff5fcfd7f9e96a23dd89a4f64d6
sia shahidi <sia.shahidi.7@gmail.com>
2023-01-29 17:27:45 +0330

    trigger sounds

--- src/App.js
+++ src/App.js
@@ -1,69 +1,84 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-const sound = [
-  {
-    keys: 'Q',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-  },
-  {
-    keys: 'W',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-  },
-  {
-    keys: 'E',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-  },
-  {
-    keys: 'A',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-  },
-  {
-    keys: 'S',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-  },
-  {
-    keys: 'D',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-  },
-  {
-    keys: 'Z',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-  },
-  {
-    keys: 'X',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-  },
-  {
-    keys: 'C',
-    sound: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-  },
-]
-
-const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-
-
 const App = () => {
+
+  const drumPads = [
+    {
+      text: 'Q',
+      keyCode: 81,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    {
+      text: 'W',
+      keyCode: 87,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+      text: 'E',
+      keyCode: 69,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+      text: 'A',
+      keyCode: 65,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+      text: 'S',
+      keyCode: 83,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+      text: 'D',
+      keyCode: 68,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+      text: 'Z',
+      keyCode: 90,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+      text: 'X',
+      keyCode: 88,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+      text: 'C',
+      keyCode: 67,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    },
+  ]
+
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    console.log(audio);
+    audio.play()
+  }
+
   return (
     <div id='drum-machine' className='container'>
       <div id='display' className='display'>
-        {keys.map((key, idx) => (
-          <Box text={key} idx={idx} />
+        {drumPads.map((sound, idx) => (
+          <div
+            className='box drum-pad'
+            text={sound.text}
+            key={idx}
+            id={sound.src}
+            onClick={() => { playSound(sound.text) }}
+          >
+            {sound.text}
+            <audio
+              className='clip'
+              id={sound.text}
+              src={sound.src}>
+            </audio>
+          </div>
         ))}
       </div>
     </div>
   );
-}
-
-class Box extends Component {
-  render() {
-    return (
-      <div className='box'>
-        {this.props.text}
-        <audio />
-      </div>
-    );
-  }
 }
 
 export default App;
